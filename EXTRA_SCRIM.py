741c5b3f96e200f7c4f992e465eca26c659d913c
faveyt <59282356+faveyt@users.noreply.github.com>
2019-12-27 18:00:09 +0700

    Update EXTRA_SCRIM.py

--- EXTRA_SCRIM.py
+++ EXTRA_SCRIM.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -10,5 +11,6 @@ async def on_ready():
     print('------------------')
     game = discord.Game('구매 문의 Fave_-#9998')
     await client.change_presence(status=discord.Status.online, activity=game)
-
-client.run('NjU4MDc0MzQzMjc1NzU3NTg5.Xf6dgg.F3n57p8ZUXbkWXJxx4omZz2dSwo')
+    
+access_token = os.environ["BOT_TOKEN"]    
+client.run(access_token)
